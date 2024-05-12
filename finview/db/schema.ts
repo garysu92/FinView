b536cb00aee20be0f27f7c9dfaa6aaf209d587e6
garysu92 <garysu929292@gmail.com>
2024-05-11 21:46:31 -0400

    drizzle ORM and NeonDB setup

---
+++ finview/db/schema.ts
@@ -0,0 +1,8 @@
+import { pgTable, text } from "drizzle-orm/pg-core";
+
+export const accounts = pgTable("accounts", {
+    id: text("id").primaryKey(),
+    name: text("name").notNull(),
+    uid: text("uid").notNull(),
+  });
+  
