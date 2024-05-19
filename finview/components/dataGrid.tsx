2038076073209b80116b29ababc8d965eaf540cd
garysu92 <garysu929292@gmail.com>
2024-05-19 00:39:29 -0400

    remove remaining card

--- finview/components/dataGrid.tsx
+++ finview/components/dataGrid.tsx
@@ -8,35 +8,38 @@ import { useGetSummary } from "@/features/summary/api/useGetSummary";
 
 import { formatDateRange } from "@/lib/utils";
 import { DataCard, DataCardLoading } from "@/components/dataCard";
+import { useEffect, useState } from "react";
 
 export const DataGrid = () => {
   const { data, isLoading } = useGetSummary();
 
   const params = useSearchParams();
-  const to = params.get("to") || undefined;
-  const from = params.get("from") || undefined;
+  const [to, setTo] = useState<string | undefined>(undefined);
+  const [from, setFrom] = useState<string | undefined>(undefined);
+  const [paramsLoaded, setParamsLoaded] = useState(false);
 
-  const dateRangeLabel = formatDateRange({ to, from });
+  useEffect(() => {
+    const toParam = params.get("to") || undefined;
+    const fromParam = params.get("from") || undefined;
+    setTo(toParam);
+    setFrom(fromParam);
+    setParamsLoaded(true);
+  }, [params]);
 
-  if (isLoading) {
+  if (!paramsLoaded || isLoading) {
     return (
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 pb-2 mb-8">
         <DataCardLoading />
         <DataCardLoading />
         <DataCardLoading />
       </div>
-    )
+    );
   }
+
+  const dateRangeLabel = formatDateRange({ to, from });
 
   return (
     <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 pb-2 mb-8">
-      <DataCard
-        title="Remaining"
-        value={data?.remainingAmount}
-        percentageChange={data?.remainingChange}
-        icon={FaPiggyBank}
-        dateRange={dateRangeLabel}
-      />
       <DataCard
         title="Income"
         value={data?.incomeAmount}
